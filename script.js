2436b60b1af433b8bf5388f75815631775785da3
rafa-almeida <rafasegt@gmail.com>
2023-06-10 21:25:34 -0300

    adicionandodesign responsivo

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ var swiper = new Swiper('.swiper', {
     keyboard: true,
 });
     
+$("#imagem").bind('mouseover',function(){
+    
+    $(this).animate({height:"200px",width:"200px"});
+
+});
+$("#imagem").bind('mouseout',function(){
+ 
+    $(this).animate({height:"76px",width:"75px"});
+   
+});
+
+
